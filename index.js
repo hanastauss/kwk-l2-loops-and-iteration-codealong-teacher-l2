35ef171d8cf563ad5a1bbfe2646396c9f6f119b3
Hana Stauss <hanastauss@icloud.com>
2018-06-21 17:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const employeeNames = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(namesArray) {
+  for (let i = 0; i < namesArray.length; i++) {
+    console.log(`Welcome ${namesArray[i]}! You are employee #${i + 1}.`) 
+  }
+  return namesArray
+}
+
+printBadges()
+
+function tailsNeverFails() {
+  let flip = Math.random()
+  let flips = 0
+  while (flip >= 0.5) {
+  ++flips
+  flip = Math.random()
+  }
+  return `You got ${flips} tails in a row!`
+}
+
